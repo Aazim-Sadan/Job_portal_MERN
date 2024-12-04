f7571ceff7c17cb92ba94c74d49db7c994fc41c6
Aazim Sadan <aazimsadan99@gmail.com>
2024-12-04 17:56:23 +0530

    models created

---
+++ backend/models/job.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    requirenents: [{
+        type: String
+    }],
+    salary: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    jobType: {
+        type: String,
+        required: true
+    },
+    position: {
+        type: Number,
+        required: true
+    },
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company',
+        requierd: true
+    },
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        requierd: true
+    },
+    applications:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Application',
+        }
+    ]
+}, {timestamps:true})
+
+export const Job = mongoose.model("Job", jobSchema);
