f1db8b384725294e63aa3e5ea6bc06f87b430e44
Aazim Sadan <aazimsadan99@gmail.com>
2024-12-14 22:37:13 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -20,13 +20,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    orgin : 'http://localhost:5173/',
-    credentials : true
-}
-
-app.use(cors(corsOptions));
-
+// const corsOptions = {
+//     orgin : 'http://localhost:5173/',
+//     credentials : true
+// }
+app.use(cors());
 
 
 // APIs
@@ -34,7 +32,7 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
-
+app.use(cors());
 
 
 
