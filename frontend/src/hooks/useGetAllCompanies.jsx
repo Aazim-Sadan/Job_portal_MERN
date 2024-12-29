c523718d2f16aa688fb8458a33deac83c70e90b3
Aazim Sadan <aazimsadan99@gmail.com>
2024-12-29 19:03:20 +0530

    fixed toast

--- frontend/src/hooks/useGetAllCompanies.jsx
+++ frontend/src/hooks/useGetAllCompanies.jsx
@@ -3,6 +3,7 @@ import { COMPANY_API_END_POINT } from '@/utils/constant'
 import axios from 'axios'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { toast } from 'sonner'
 
 const useGetAllCompanies = () => {
 
