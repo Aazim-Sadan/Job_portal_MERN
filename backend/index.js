1f95e1b4b2bf180c6cb75e297a6646152390c010
Aazim Sadan <aazimsadan99@gmail.com>
2024-12-29 19:40:49 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -15,34 +15,15 @@ dotenv.config({});
 const app = express();
 
 
-// const corsOptions = {
-//     origin: 'http://localhost:5173',
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-// };
-// app.use(cors(corsOptions));
-
 const corsOptions = {
-    origin: function (origin, callback) {
-        const allowedOrigins = [
-            'http://localhost:5173', // Local development
-            'https://job-portal-mern-qz2a.vercel.app', // Production frontend
-        ];
-
-        // If no origin (e.g., server-to-server requests) or origin is in the allowed list
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true, // To allow cookies/authorization headers
+    // origin: 'http://localhost:5173',
+    origin: 'https://job-portal-mern-qz2a.vercel.app',
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
-
 app.use(cors(corsOptions));
+
 
 
 // middleware
