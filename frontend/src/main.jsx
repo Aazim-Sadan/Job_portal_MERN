55ae5877f10e85031c8d34c3ed3a0009a50d1d1d
Aazim Sadan <aazimsadan99@gmail.com>
2024-12-29 18:43:50 +0530

    add strict mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -10,12 +11,12 @@ import { PersistGate } from 'redux-persist/integration/react'
 const persister = persistStore(store);
 
 createRoot(document.getElementById('root')).render(
-  <>
+  <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persister}>
         <App />
       </PersistGate>
     </Provider>
     <Toaster />
-  </>
+  </StrictMode>
 )
