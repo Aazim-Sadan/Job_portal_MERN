f051c0bc85e52abda5dbaed61453dfb7629ad2a3
Aazim Sadan <aazimsadan99@gmail.com>
2024-12-30 20:03:28 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -7,17 +7,20 @@ import userRoute from "./routes/user.route.js"
 import companyRoute from "./routes/company.route.js"
 import jobRoute from "./routes/job.route.js"
 import applicationRoute from "./routes/application.route.js"
+import path from "path";
 
 
 
 dotenv.config({});
-
 const app = express();
+
+const _dirname = path.resolve();
+
 
 
 const corsOptions = {
-    // origin: 'http://localhost:5173',
-    origin: 'https://job-portal-mern-qz2a.vercel.app',
+    origin: 'http://localhost:5173',
+    // origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
@@ -39,6 +42,10 @@ app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+})
 
 
 
