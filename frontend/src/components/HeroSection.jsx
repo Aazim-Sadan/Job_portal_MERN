40f2bf0ccd79349b742b29cebbd6cb8a9c5f6a52
Aazim Sadan <aazimsadan99@gmail.com>
2024-12-31 20:44:01 +0530

    title change

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -19,7 +19,7 @@ const HeroSection = () => {
     return (
         <div className='px-3 sm:text-center'>
             <div className='flex flex-col gap-5 my-10'>
-                <span className='mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-normal text-sm sm:text-[15px]'>No. 1 Job Hunt Website</span>
+                <span className='mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-normal text-sm sm:text-[15px]'>A buzzing hub for job opportunities</span>
                 <h1 className='text-4xl sm:text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Job</span></h1>
                 <p>Find your next opportunity with ease - connect, apply, and thrive in your dream career</p>
                 <div className=' flex w-full sm:w-[40%] shadow-lg border-gray-200 pl-4 rounded-full items-center gap-4 mx-auto'>
--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
         <div className=' bg-white mx-2'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
                 <div >
-                    <h1 className='text-2xl font-bold'> <Link to="/">Job<span className='text-[#F83002]'>Portal</span></Link></h1>
+                    <h1 className='text-2xl font-bold' > <Link to="/">Work<span className='text-[#F83002]'>Hive</span></Link></h1>
                 </div>
                 <div className='flex items-center gap-4 sm:gap-12'>
                     <ul className='flex font-medium items-center gap-3 sm:gap-5'>
