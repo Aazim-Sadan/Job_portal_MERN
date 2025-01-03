31095103d61aded67121abd413bddf648c7c7359
Aazim Sadan <aazimsadan99@gmail.com>
2025-01-03 23:44:12 +0530

    changes in saved jobs components

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -42,7 +42,6 @@ const Job = ({ job }) => {
 
             <div>
                 <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
-                {/* <p className='text-sm text-gray-600'>{job?.description}</p> */}
                 <p className='text-sm text-gray-600 overflow-hidden text-ellipsis'>
                     {job?.description?.length > 100 ? `${job?.description.slice(0, 78)}...` : job?.description}
                 </p>
--- frontend/src/components/SavedJobsCard.jsx
+++ frontend/src/components/SavedJobsCard.jsx
@@ -18,7 +18,10 @@ const SavedJobsCard = ({ job, deleteJob }) => {
 
             <div>
                 <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
-                <p className='text-sm text-gray-600'>{job?.description}</p>
+                {/* <p className='text-sm text-gray-600'>{job?.description}</p> */}
+                <p className='text-sm text-gray-600 overflow-hidden text-ellipsis'>
+                    {job?.description?.length > 100 ? `${job?.description.slice(0, 78)}...` : job?.description}
+                </p>
             </div>
 
             <div className='flex items-center gap-2 mt-4 '>
